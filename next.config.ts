80b14d357b86d4caf92c5d764229f10ac09a6f80
Deep Kevadiya <deepkevadiya18@gmail.com>
2026-07-04 22:33:57 +0530

    Format import and export statements in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import  type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true }
 };
 
-export default nextConfig;
+export  default nextConfig;
