4b678059f4a6ea10e6b6b671cdadb6bfa6e923e3
Deep7179 <deepkevadiya18@gmail.com>
2026-07-04 12:44:53 +0530

    fix api url issue

--- lib/api.ts
+++ lib/api.ts
@@ -1,25 +1,7 @@
 import axios from "axios";
 
-const BASE_URL =
-  typeof window !== "undefined"
-    ? process.env.NEXT_PUBLIC_API_URL
-    : "";
-
 const api = axios.create({
-  baseURL: BASE_URL ? `${BASE_URL}/api` : "/api",
-});
-
-api.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers = config.headers || {};
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-
-  return config;
+  baseURL: "https://problem-tracker-zww7.onrender.com/api",
 });
 
 export default api;
